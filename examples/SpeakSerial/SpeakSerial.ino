64b5609a4828cb1c980d6e1bc97718bf21cac5c0
Stephen Crane <jscrane@gmail.com>
2018-04-07 13:13:04 +0100

    use LED_BUILTIN

--- examples/SpeakSerial/SpeakSerial.ino
+++ examples/SpeakSerial/SpeakSerial.ino
@@ -5,7 +5,7 @@ TTS tts(3);
 
 void setup(void) {
   Serial.begin(57600);
-  pinMode(13, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 char line[80];
@@ -15,7 +15,7 @@ void loop(void) {
   if (Serial.available() > 0) {
     char c = (char)Serial.read();
     if (c == '\n') {
-      digitalWrite(13, HIGH);
+      digitalWrite(LED_BUILTIN, HIGH);
       line[pos++] = 0;
       if (line[0] == '+') {
         int p = atoi(line+1);
@@ -25,7 +25,7 @@ void loop(void) {
         tts.sayText(line);
       }
       pos = 0;
-      digitalWrite(13, LOW);
+      digitalWrite(LED_BUILTIN, LOW);
     } else
       line[pos++] = c;
   }
